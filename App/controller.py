9fd4c457ee0af2399a9e3cc4128a78ebbbdd8e41
Santiago Felipe Arteaga Martin <sa-artea@uniandes.edu.co>
2020-11-03 16:51:34 -0500

    preparacion de template

--- App/controller.py
+++ App/controller.py
@@ -41,95 +41,11 @@ recae sobre el controlador.
 # ___________________________________________________
 
 
-def init():
-    """
-    Llama la funcion de inicializacion  del modelo.
-    """
-    # analyzer es utilizado para interactuar con el modelo
-    analyzer = model.newAnalyzer()
-    return analyzer
-
-
 # ___________________________________________________
 #  Funciones para la carga de datos y almacenamiento
 #  de datos en los modelos
 # ___________________________________________________
 
-
-def loadServices(analyzer, servicesfile):
-    """
-    Carga los datos de los archivos CSV en el modelo.
-    Se crea un arco entre cada par de estaciones que
-    pertenecen al mismo servicio y van en el mismo sentido.
-
-    addRouteConnection crea conexiones entre diferentes rutas
-    servidas en una misma estación.
-    """
-    servicesfile = cf.data_dir + servicesfile
-    input_file = csv.DictReader(open(servicesfile, encoding="utf-8"),
-                                delimiter=",")
-    lastservice = None
-    for service in input_file:
-        if lastservice is not None:
-            sameservice = lastservice['ServiceNo'] == service['ServiceNo']
-            samedirection = lastservice['Direction'] == service['Direction']
-            if sameservice and samedirection:
-                model.addStopConnection(analyzer, lastservice, service)
-        lastservice = service
-    model.addRouteConnections(analyzer)
-    return analyzer
-
 # ___________________________________________________
 #  Funciones para consultas
 # ___________________________________________________
-
-
-def totalStops(analyzer):
-    """
-    Total de paradas de autobus
-    """
-    return model.totalStops(analyzer)
-
-
-def totalConnections(analyzer):
-    """
-    Total de enlaces entre las paradas
-    """
-    return model.totalConnections(analyzer)
-
-
-def connectedComponents(analyzer):
-    """
-    Numero de componentes fuertemente conectados
-    """
-    return model.connectedComponents(analyzer)
-
-
-def minimumCostPaths(analyzer, initialStation):
-    """
-    Calcula todos los caminos de costo minimo de initialStation a todas
-    las otras estaciones del sistema
-    """
-    return model.minimumCostPaths(analyzer, initialStation)
-
-
-def hasPath(analyzer, destStation):
-    """
-    Informa si existe un camino entre initialStation y destStation
-    """
-    return model.hasPath(analyzer, destStation)
-
-
-def minimumCostPath(analyzer, destStation):
-    """
-    Retorna el camino de costo minimo desde initialStation a destStation
-    """
-    return model.minimumCostPath(analyzer, destStation)
-
-
-def servedRoutes(analyzer):
-    """
-    Retorna el camino de costo minimo desde initialStation a destStation
-    """
-    maxvert, maxdeg = model.servedRoutes(analyzer)
-    return maxvert, maxdeg
